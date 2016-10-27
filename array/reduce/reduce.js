a45189f0915039064880816d6c607a33ced69e94
jjsub <jsamesub@gmail.com>
2016-10-27 13:58:03 -0500

    Array reduce

---
+++ array/reduce/reduce.js
@@ -0,0 +1,19 @@
+'strict mode';
+
+let fla = (name) => {
+  console.log(name);
+}
+
+fla('Fern')
+
+
+
+// const output = fs.readFileSync('data.txt', 'UTF8')
+//   .trim()
+//   .split('\n')
+//   .map((line) => line.split('\t'))
+//   .reduce((customers, [name, order, price, quantity]) => {
+//     customers[name] = customers[name] || [];
+//     customers[name].push({ order, price, quantity });
+//     return customers;
+//   }, {});﻿
